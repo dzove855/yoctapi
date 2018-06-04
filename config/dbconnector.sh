acf6d540098099991550714508d9cce459d1066d
dzove855 <v.dzogovic@flash-global.net>
2018-06-04 18:50:32 +0200

    Update dbconnector.sh

--- config/dbconnector.sh
+++ config/dbconnector.sh
@@ -4,14 +4,14 @@ declare -A DB_DELETE
 declare -A DB_POST
 declare -A DB_CONNECT
 
-DB_CONNECT['config':'slave_mysql_dbname']="yoconf"
-DB_CONNECT['config':'slave_mysql_host']="localhost"
-DB_CONNECT['config':'slave_mysql_user']="root"
-DB_CONNECT['config':'slave_mysql_port']="3306"
-DB_CONNECT['config':'slave_mysql_password']="trakto"
+DB_CONNECT['table':'slave_mysql_dbname']="dbname"
+DB_CONNECT['table':'slave_mysql_host']="host"
+DB_CONNECT['table':'slave_mysql_user']="user"
+DB_CONNECT['table':'slave_mysql_port']="3306"
+DB_CONNECT['table':'slave_mysql_password']="password"
 
-DB_GET['config':'where']="service"
+DB_GET['table':'where']="service"
 
 ### Update ###
-DB_PUT['config':'where']="id"
-DB_DELETE['config':'where']="id"
+DB_PUT['table':'where']="id"
+DB_DELETE['table':'where']="id"
