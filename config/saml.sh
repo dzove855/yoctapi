62ba77de32c8dcd1ae4c050e7f3485c0cdd391c1
dzove855 <dzove855@gmail.com>
2021-12-23 09:37:11 +0100

    Delete saml.sh

--- config/saml.sh
+++
@@ -1,4 +0,0 @@
-_saml_host_url="HOST"
-_saml_xml_template="/var/tmp/esm/config/saml/template/auth.xml"
-_saml_idp_xml="/var/tmp/esm/config/saml/idp/idp.xml"
-_saml_priv_key="/var/tmp/esm/config/saml/keys/private.key"
